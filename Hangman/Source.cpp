73fbc9b0229998a77b69cfed3a11a223bdb4ba04
hachlol <hachlol@student.uzlabina.cz>
2024-06-02 20:05:57 +0200

    generating random words and signs of game loop

--- Hangman/Source.cpp
+++ Hangman/Source.cpp
@@ -3,16 +3,43 @@
 #include <stdlib.h>
 #include <time.h>
 
+int lives = 10;
 
+const char* randomWord()
+{
+	int random = rand() % (6);
 
+	switch (random)
+	{
+	case 0:
+		return "folbr";
+	case 1:
+		return "software";
+	case 2:
+		return "programovani";
+	case 3:
+		return "javascript";
+	case 4:
+		return "uzlabina";
+	case 5:
+		return "hardware";
+	}
+
+}
 
 int main()
 {
+
 	time_t t;
 	srand((unsigned)time(&t));
-
-	int randWord = rand() % (6);
-	printf("%d ", randWord);
+	for (int i = 0; i < 26; i++)
+	{
+		printf("%s \n", randomWord());
+	}
+		
+	
+	
+	
 
 	return 0;
 }
