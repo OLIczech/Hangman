d4ac5b287b75d3326c08dadfbcbd109ef1c4b3dd
hachlol <hachlol@student.uzlabina.cz>
2024-06-04 20:02:51 +0200

    ascii art function

--- Hangman/Source.cpp
+++ Hangman/Source.cpp
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int lives = 10;
+
 
 const char* randomWord()
 {
@@ -27,19 +29,122 @@ const char* randomWord()
 
 }
 
+
+void asciiArt(int lives)
+{
+	switch (lives)
+	{
+	case 9:
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+		case 8:
+			printf(" _____________\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+		case 7:
+			printf(" _____________\n");
+			printf("    | /\n");
+			printf("    |/\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+		case  6:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+		case 5:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+		case 4:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |         |\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+		case 3:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |        /|\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+		case 2:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |        /|\\\n");
+			printf("    |\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+		case 1:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |        /|\\\n");
+			printf("    |        /\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+		case 0:
+			printf(" _____________\n");
+			printf("    | /       |\n");
+			printf("    |/        0\n");
+			printf("    |        /|\\\n");
+			printf("    |        / \\\n");
+			printf("    |\n");
+			printf("    |\n");
+			break;
+
+	}
+}
+
+
+
 int main()
 {
-
+	
+	
 	time_t t;
 	srand((unsigned)time(&t));
 	for (int i = 0; i < 26; i++)
 	{
 		printf("%s \n", randomWord());
+		asciiArt(i);
 	}
-		
-	
-	
-	
 
 	return 0;
 }
